8f3bf708a89a5ab7c42b6c65b4f0651b2674535d
michael1342 <michaelanazodo2024@gmail.com>
2026-03-12 09:46:19 +0100

    I found the criminal

---
+++ array-assignment.js
@@ -0,0 +1,45 @@
+const places = ["market", "church", "office", "mall","gym", "bank", "saloon", "eatery", "spa", "club", "office", "airport", "gym", "eatery", "market", "office", "mall", "eatery", "spa", "market", "eatery", "saloon", "church", "office", "spa", "club", "office", "office", "mall", "eatery", "gym", "bank", "saloon", "airport", "gym","eatery" ]
+
+
+
+const button = document.getElementById('btn')
+const input = document.getElementById('place')
+const log = document.getElementById('log')
+
+
+    
+const checkPlaces = (place, visit) => {
+    const places = ["market", "church", "office", "mall","gym", "bank", "saloon", "eatery", "spa", "club", "office", "airport", "gym", "eatery", "market", "office", "mall", "eatery", "spa", "market", "eatery", "saloon", "church", "office", "spa", "club", "office", "office", "mall", "eatery", "gym", "bank", "saloon", "airport", "gym","eatery" ]
+
+    const input = document.getElementById('place')
+    if (place) {
+        console.log(`visited ${input.value} ${visit} times`)
+        log.textContent = `visited ${input.value} ${visit} times`
+    }
+
+    //places.includes()
+
+    if (places.includes(input.value)) {
+        console.log('ayy')
+    } else {
+        console.log('Not Found in Database')
+        log.textContent = 'Not Found in Database'
+    }
+}
+
+button.addEventListener('click', () => {
+
+    places.forEach((place, index) => {
+        
+})
+
+const searchTerm = input.value.toLowerCase().trim();
+    
+    // Calculate the total visits once
+    const visitCount = places.filter(p => p === searchTerm).length;
+
+checkPlaces(input, visitCount)
+ 
+})
+
+  
